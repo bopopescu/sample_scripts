10fb58410233ad29feaeb23288f58882d2ea101a
Suhaib Chishti <Suhaib.Chishti@exponential.com>
2018-06-07 13:08:27 -0700

    Added a comment for changes made to the file

--- horizon/openstack_dashboard/dashboards/project/volumes/volumes/forms.py
+++ horizon/openstack_dashboard/dashboards/project/volumes/volumes/forms.py
@@ -12,6 +12,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+# Comment: File has hardcoded values for number of volumes per user and size of volume
 
 """
 Views for managing volumes.
